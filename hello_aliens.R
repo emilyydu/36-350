646603f5b37ec22fe19e43220edaba392e9701f0
Emily Du <emilydu@emilys-mbp-3.attlocal.net>
2020-07-30 15:47:09 -0700

    message to aliens

---
+++ hello_aliens.R
@@ -0,0 +1,13 @@
+cat("Hello, aliens!
+    
+    This message is coming from planet Earth. We have detected your planet and 
+    have discovered that it is in the habitable zone and similar to our planet. 
+    What is your planet like? What species live on your planet? Do you know of 
+    any other planets around yours that are also habitable? Our planet has 
+    millions of different species and many types of terrains. There are no 
+    habitable planets near ours, and your planet is the first that we have found 
+    that is. Please respond to our message as soon as possible, and we hope to 
+    keep in contact!
+    
+    Sincerely,
+    Earth")
